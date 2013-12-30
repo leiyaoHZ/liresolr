87ee08fab276fae174357184a75a9d611c306df1
Nurul Ferdous <nurul@tasawr.com>
2013-12-30 23:38:34 +0600

    fix

--- deleteAll.sh
+++ deleteAll.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl http://localhost:8888/solr/images/update  -H "Content-Type: text/xml" --data-binary "<delete><query>*:*</query></delete>"
+curl http://localhost:8888/solr/images/update  -H "Content-Type: text/xml" --data-binary "<delete><query>{!raw f=id}file:/Users/ferdous/projects/digitalcandy/datam/data/thumbnails/.DS_Store</query></delete>"
 curl http://localhost:8888/solr/images/update  -H "Content-Type: text/xml" --data-binary "<commit/>"
